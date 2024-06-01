8bd9ca16598e6d849202caee70f495c021f0b8aa
Willv97 <willv1357@gmail.com>
2024-06-01 00:09:25 -0400

    Added link to js file in index.html, added comments

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// Get the button
 var topButton = documet.getElementById("top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topButton.style.display = "block";
@@ -7,7 +11,9 @@ function scrollFunction() {
         topButton.style.display = "none";
     }
 }
+
+//When the user clicks on the button, scroll to the top
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0; //For safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
